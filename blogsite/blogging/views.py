f22f16e748702a8bfd9121a3056e22d66e66bd76
kiljoy001 <kiljoy001@gmail.com>
2021-05-23 18:46:04 -0700

    added lists view

--- blogsite/blogging/views.py
+++ blogsite/blogging/views.py
@@ -1,4 +1,13 @@
-from django.shortcuts import HttpResponse, HttpResponseRedirect, Http404
+from django.shortcuts import HttpResponse, HttpResponseRedirect, Http404, render
+from django.template import loader
+from blogging.models import Post
+
+
+def list_view(request):
+    published = Post.objects.exclude(published_date__exact=None)
+    posts = published.order_by('-published_date')
+    context = {'posts': posts}
+    return render(request, 'blogging/list.html', context)
 
 
 def stub_view(request, *args, **kwargs):
